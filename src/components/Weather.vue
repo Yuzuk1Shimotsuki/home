ac8906f57ee9e9f699cf82cfb7aee65855860edd
霜月柚希 🍊 <chinggoddon5838@gmail.com>
2025-03-25 22:27:03 +0800

    refactor: Translate Weather.vue

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -5,15 +5,15 @@
     <span>{{ weatherData.weather.temperature }}℃</span>
     <span class="sm-hidden">
       &nbsp;{{
-        weatherData.weather.winddirection?.endsWith("风")
+        weatherData.weather.winddirection?.endsWith("風")
           ? weatherData.weather.winddirection
-          : weatherData.weather.winddirection + "风"
+          : weatherData.weather.winddirection + "風"
       }}&nbsp;
     </span>
-    <span class="sm-hidden">{{ weatherData.weather.windpower }}&nbsp;级</span>
+    <span class="sm-hidden">{{ weatherData.weather.windpower }}&nbsp;級</span>
   </div>
   <div class="weather" v-else>
-    <span>天气数据获取失败</span>
+    <span>天氣數據獲取失敗</span>
   </div>
 </template>
 
@@ -21,46 +21,46 @@
 import { getAdcode, getWeather, getOtherWeather } from "@/api";
 import { Error } from "@icon-park/vue-next";
 
-// 高德开发者 Key
+// 高德開發者 Key
 const mainKey = import.meta.env.VITE_WEATHER_KEY;
 
-// 天气数据
+// 天氣數據
 const weatherData = reactive({
   adCode: {
     city: null, // 城市
-    adcode: null, // 城市编码
+    adcode: null, // 城市編碼
   },
   weather: {
-    weather: null, // 天气现象
-    temperature: null, // 实时气温
-    winddirection: null, // 风向描述
-    windpower: null, // 风力级别
+    weather: null, // 天氣現象
+    temperature: null, // 實時氣溫
+    winddirection: null, // 風向描述
+    windpower: null, // 風力級別
   },
 });
 
-// 取出天气平均值
+// 取出天氣平均值
 const getTemperature = (min, max) => {
   try {
-    // 计算平均值并四舍五入
+    // 計算平均值並四捨五入
     const average = (Number(min) + Number(max)) / 2;
     return Math.round(average);
   } catch (error) {
-    console.error("计算温度出现错误：", error);
+    console.error("計算溫度出現錯誤：", error);
     return "NaN";
   }
 };
 
-// 获取天气数据
+// 獲取天氣數據
 const getWeatherData = async () => {
   try {
-    // 获取地理位置信息
+    // 獲取地理位置信息
     if (!mainKey) {
-      console.log("未配置，使用备用天气接口");
+      console.log("未配置，使用備用天氣接口");
       const result = await getOtherWeather();
       console.log(result);
       const data = result.result;
       weatherData.adCode = {
-        city: data.city.City || "未知地区",
+        city: data.city.City || "未知地區",
         // adcode: data.city.cityId,
       };
       weatherData.weather = {
@@ -70,17 +70,17 @@ const getWeatherData = async () => {
         windpower: data.condition.day_wind_power,
       };
     } else {
-      // 获取 Adcode
+      // 獲取 Adcode
       const adCode = await getAdcode(mainKey);
       console.log(adCode);
       if (adCode.infocode !== "10000") {
-        throw "地区查询失败";
+        throw "地區查詢失敗";
       }
       weatherData.adCode = {
         city: adCode.city,
         adcode: adCode.adcode,
       };
-      // 获取天气信息
+      // 獲取天氣信息
       const result = await getWeather(mainKey, weatherData.adCode.adcode);
       weatherData.weather = {
         weather: result.lives[0].weather,
@@ -90,12 +90,12 @@ const getWeatherData = async () => {
       };
     }
   } catch (error) {
-    console.error("天气信息获取失败:" + error);
-    onError("天气信息获取失败");
+    console.error("天氣信息獲取失敗:" + error);
+    onError("天氣信息獲取失敗");
   }
 };
 
-// 报错信息
+// 報錯信息
 const onError = (message) => {
   ElMessage({
     message,
@@ -108,7 +108,7 @@ const onError = (message) => {
 };
 
 onMounted(() => {
-  // 调用获取天气
+  // 調用獲取天氣
   getWeatherData();
 });
 </script>
